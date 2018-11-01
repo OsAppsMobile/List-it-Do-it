76d195ec11c9c1d252e8f58b9ea90d329dad0598
Osman Donmez <osmandnmez@gmail.com>
2018-11-01 09:54:52 +0300

    View and VCs created. Pods are installed.

---
+++ List it Do it/Controller/ListItVC.swift
@@ -0,0 +1,42 @@
+//
+//  ListItVC.swift
+//  List it Do it
+//
+//  Created by Osman Dönmez on 1.11.2018.
+//  Copyright © 2018 Osman Dönmez. All rights reserved.
+//
+
+import UIKit
+
+class ListItVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+
+
+}
